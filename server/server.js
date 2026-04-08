2719090bbc23d6ed9669651c6c5e93ccfd342404
Sunny <sunny@example.com>
2026-04-09 00:21:24 +0530

    fix cors and express wildcard error

--- server/server.js
+++ server/server.js
@@ -1,34 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import { connect } from 'mongoose';
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoutes.js';
 import ownerRouter from './routes/ownerRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 
-//Initialize express app
 const app = express();
 
-//Connect to MongoDB
 await connectDB();
 
-
-//Middleware
-
-
 app.use(cors({
-    origin: "https://car-rental-website-olive-gamma.vercel.app",
+    origin: ['http://localhost:5173', 'https://car-rental-website-olive-gamma.vercel.app'],
     credentials: true
 }));
-app.options("/*", cors());
+
 app.use(express.json());
 
-
-//Routes
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
 app.use('/api/user', userRouter);
 app.use('/api/owner', ownerRouter);
 app.use('/api/bookings', bookingRouter);
