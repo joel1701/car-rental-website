79bea70e309392155e2aec135e32185385244f04
Sunny <sunny@example.com>
2026-04-09 01:12:10 +0530

    fixing search bar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -59,14 +59,14 @@ export const Navbar = () => {
                     </Link>
                 ))}
 
-                <div className="hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
+                {/* <div className="hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
                     <input
                         type="text"
                         className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
                         placeholder="Search products"
                     />
                     <img src={assets.search_icon} alt="search" />
-                </div>
+                </div> */}
 
                 <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
 
